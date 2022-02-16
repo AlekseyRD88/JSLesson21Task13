b06aac8b3ce0861f31dfdb766290ebace7557e5b
= <neymegen50@gmail.com>
2022-02-16 11:54:34 +0200

    message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export function getSection(num) {
   let elem = document.querySelector(`span[data-number="${num}"]`);
-  let parent = elem.closest('box');
+  let parent = elem.closest('.box');
   return parent.dataset.section;
 }
